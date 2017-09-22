a2a18c814dee05bbea9a4e631766caba0c5e10df
Mihai Chifor <chifor.mihai21@gmail.com>
2017-09-22 15:42:41 +0300

    update styleguide config

--- styleguide.config.js
+++ styleguide.config.js
@@ -22,5 +22,28 @@ module.exports = {
       content: 'docs/ui.md',
       components: 'src/components/*.js'
     }
-  ]
+  ],
+  getExampleFilename: function(componentPath) {
+    return componentPath.replace(/\.jsx?$/,   '.md');
+  },
+  styleguideDir: 'styleguide',
+  updateWebpackConfig: function(webpackConfig, env) {
+    webpackConfig.module.resolve = {
+      extensions: [
+        '',
+        '.js',
+        '.jsx'
+      ]
+    };
+    webpackConfig.module.loaders = webpackConfig.module.loaders.concat([
+      // Babel loader will use your project’s .babelrc
+      {
+        test: /\.jsx?$/,
+        include: dir,
+        loader: 'babel'
+      }
+    ]);
+
+    return webpackConfig;
+  }
 };
